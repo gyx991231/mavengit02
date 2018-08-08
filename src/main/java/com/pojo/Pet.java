e2a86e6a2d8e588f210251c36aa76e8c1b5f4b83
gyx991231 <42162652+gyx991231@users.noreply.github.com>
2018-08-08 15:40:49 +0800

    Update Pet.java
    描述不可用

--- src/main/java/com/pojo/Pet.java
+++ src/main/java/com/pojo/Pet.java
@@ -3,6 +3,8 @@ package com.pojo;
 public class Pet {
     public  void show(){
         System.out.println("123");
-        System.out.println("456789");
+        System.out.println("456789");//新增的代码不符合要求，请重新修改
+        
+        
     }
 }
